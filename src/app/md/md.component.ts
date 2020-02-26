4b1293018f664b667e17a46ee7b980f9c0b3b564
Fabiana Campos <faby@MacBook-Pro-de-Fabiana.local>
2020-02-26 11:01:11 -0300

    correções Beto

--- src/app/md/md.component.ts
+++ src/app/md/md.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { LinhaService } from '../linhas/linha.service';
-import { Observable } from 'rxjs';
-import { Linha } from '../linhas/linha';
+import { Component, OnInit } from "@angular/core";
+import { LinhaService } from "../linhas/linha.service";
+import { Observable } from "rxjs";
+import { Linha } from "../linhas/linha";
 
 @Component({
-  selector: 'app-md',
-  templateUrl: './md.component.html',
-  styleUrls: ['./md.component.css']
+  selector: "app-md",
+  templateUrl: "./md.component.html",
+  styleUrls: ["./md.component.css"]
 })
 export class MdComponent implements OnInit {
   linhas: Observable<Linha>;
-
-  constructor(private linhaservice: LinhaService,) { }
+  linha: Linha = new Linha();
+  submitted = false;
+  constructor(private linhaservice: LinhaService) {}
 
   ngOnInit() {
     this.linhas = this.linhaservice.getLinhaList();
+    console.log(this.linhas);
   }
 
+  onSubmit() {
+    this.submitted = true;
+  }
 }
